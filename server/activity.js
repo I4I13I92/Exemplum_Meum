6b2ce9c3842574cc7367e327dc91d0f8e3c6457c
Lavic13 <vmoo1392@gmail.com>
2019-12-25 22:17:18 -0800

    completed basic file server, need to connect FE to BE

---
+++ server/activity.js
@@ -0,0 +1,47 @@
+// Activity class
+module.exports = class activity{
+
+	//class constructor, mostly defined by Date object upon instantiating time
+	//gets type from user/front-end
+	constructor(type, duration){
+
+
+		this.id = activity.count++;
+		this.type = type;
+		this.duration = duration;
+		this.today_date = new Date();
+		this.month = this.today_date.getMonth() + 1;
+		this.day = this.today_date.getDay();
+		this.date = this.today_date.getDate();
+		this.year = this.today_date.getFullYear();
+	}
+
+	static count = 0;
+
+	//set the event type based on the user's input
+	set_type(event_type)
+	{
+		this.type = event_type;
+	}
+
+	//print info on specific event
+	print_Info(activity) 
+	{	
+		return `You spent ${this.duration} minute(s) ${this.type} on ${this.month}-${this.date}-${this.year}.`;
+	}
+
+	//return the date of the event based on when the object is created in mm/dd/yyyy format
+	date_created()
+	{
+		return `${this.month}-${this.date}-${this.year}`;
+	}
+
+	//return id
+	get_Id()
+	{
+		return `${this.id}`;
+	}
+		
+}
+
+
--- server/src/index.js
+++ server/src/index.js
@@ -3,24 +3,26 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const fs  = require('fs');
+const activity  = require('../activity');
 
 
 const app = express();
-let count = 0;
-let d = new Date();
-
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) =>{
-	fs.stat('stuff', (err, stats) => 
+
+	let act = new activity('sleeping', 45);
+
+	fs.stat(act.date_created(), (err, stats) => 
 	{
+		//check if current foloder exists, if true, write file containing data
 		if (!err) 
 		{
 			console.log('file or directory exits');
-			fs.writeFile('stuff/' + d.getTime().toString(), count++, (err) =>
+			fs.writeFile(act.date_created() + '/'+ act.get_Id(), JSON.stringify(act), (err) =>
 			{
 				if (err) throw err;
 				console.log('Saved');
@@ -28,20 +30,22 @@ app.get('/', (req, res) =>{
 		}
 		else
 		{
+			//create directory if it doesnt exist, using today's date as a name
 			console.log('file/directory does not exist');
-			fs.mkdir('stuff', function()
+			fs.mkdir(act.date_created(), function()
 			{
+				fs.writeFile(act.date_created() + '/'+ act.get_Id(), JSON.stringify(act), (err) =>
+				{
+					if (err) throw err;
+					console.log('Saved');
+				})
 
 			});
-			console.log('directory stuff has been made')
+			console.log('directory stuff has been made');
 		}
 	})
-	/*let month = (d.getMonth() + 1).padStart(2, '0');
-	let day = d.getDate().padStart(2, '0');
-	let year = d.getFullYear();*/
-
 	res.send({
-		message: (d.getMonth() + 1) +'-'+ d.getDate() +'-' + d.getFullYear()
+		message: act.print_Info() +" " + act.get_Id()
 	})
 })
 
