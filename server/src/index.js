398197babeb137947a2478e3ab186c11f8184cfc
Lavic13 <vmoo1392@gmail.com>
2020-02-04 21:56:56 -0800

    need to figure out blank writefile issue

--- server/src/index.js
+++ server/src/index.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const fs  = require('fs');
-const activity  = require('../stats_class/activity');
-const stats = require('../stats_class/dailyStats');
+const activity  = require("C:/Users/Victor/VueJs/exemplum_meum/Exemplum_Meum/server/src/stats_class/activity");
+const stats = require("C:/Users/Victor/VueJs/exemplum_meum/Exemplum_Meum/server/src/stats_class/dailyStats");
 
 const app = express();
 
@@ -12,21 +12,18 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/a/:act', (req, res) => {
 //for testing purposes
 	let minutes = [10, 20, 30, 40, 50, 60, 70];
-	let activities = ['running', 'sleeping', 'eating', 'programming', 'napping', 'lifting', 'reading', 'cleaning'];
-	let min_count = 0;
-	let act_count = 0;
-
-	let curr_act = new activity(activity[act_count++], minutes[min_count++])
-	//curr_act.write_Activity()
+	let min = minutes[2];
 
 
-	if (min_count === 6) { min_count = 0};
-	if (act_count === 7) { act_count = 0};
+	let curr_act = new activity(req.params.act, min)
+	curr_act.write_Activity()
+	//console.log(typeof(min));
 
-	res.send({ message: {$curr_act})
+
+	res.send({ message: curr_act })
 })
 
 app.post('/astroserver', (req, res) => {
--- server/src/stats_class/stats_calculator.js
+++ server/src/stats_class/stats_calculator.js
@@ -1,9 +1,10 @@
-const fs = require('fs');
+const fs = require('fs').promises;
+const FS = require('fs');//clean up
 const path = require('path');
 const util = require('util');
 
-const readfile = util.promisify(fs.readFile);
-const writefile = util.promisify(fs.writeFile);
+//const readfile = util.promisify(fs.readFile);
+//const writefile = util.promisify(fs.writeFile);
 
 const imports = require('./imports.js');
 
@@ -38,7 +39,7 @@ const get_daily_stats = (state) => ({
 		const file_name = 'activity_sums';
 		const stats_obj_file = path.join(imports.my_path(), date, file_name);
 		//read file and parse json into map
-		let daily_stats = JSON.parse(await readfile(stats_obj_file));
+		let daily_stats = JSON.parse(await fs.readFile(stats_obj_file));
 		state.activity_minutes = new Map(Object.entries(daily_stats));
 
 	}
@@ -49,29 +50,41 @@ const update_daily_stats = (state) => ({
 	{
 		const file_name = 'activity_sums';
 		const activities_path = path.join(path_to_month, file_name);
-		//console.log(activities_path);
+		console.log(activities_path);
 
 		//read from file, parse json, and convert into a map
-		//let unparsed_data = await readfile(activities_path);
-		//console.log(unparsed_data);
-		//let day_stats = JSON.parse(unparsed_data);
-		//let day_stats = JSON.parse(await readfile(activities_path));
-		/*let update_day_stats_map = new Map(Object.entries(day_stats));
+		let unparsed_data = await fs.readFile(activities_path);
+		console.log(unparsed_data);
+		let day_stats = JSON.parse(unparsed_data);
+		console.log(day_stats);
+		console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		let update_day_stats_map = new Map(Object.entries(day_stats));
+		console.log(update_day_stats_map);
+		console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		update_day_stats_map.set(state.type, state.duration);
+		console.log(update_day_stats_map);
 
+		//await fs.open(activities_path);
+		FS.writeFileSync(activities_path, JSON.stringify(update_day_stats_map));
+		console.log("completed file write");
 		//update the new activity to include into new data
-		if(update_day_stats_map.has(state.type)) 
+		/*if(update_day_stats_map.has(state.type)) 
 		{
 			let update_activity_day_minutes = Number(update_day_stats_map.get(state.type));
 			update_activity_day_minutes += state.duration;
 			update_day_stats_map.set(state.type, update_activity_day_minutes); 
+			console.log(update_day_stats_map);
 		}
 		else
 		{
 			update_day_stats_map.set(state.type, state.duration);
+			console.log(update_day_stats_map);
 		}
-
+		*/
 		//write updated stats to file server
-		await fs.writeFile(activities_path, JSON.stringify(update_day_stats_map));*/
+		//update_day_stats_map.set(state.type, state.duration);
+		//console.log(update_day_stats_map + '!!!!!!!!!!!!!!!!!!!!');
+		//await fs.writeFile(activities_path, JSON.stringify(update_day_stats_map));
 	}
 })
 
