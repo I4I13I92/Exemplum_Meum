65092bd28319287f28d81efeb16e93a7d7c3e967
Lavic13 <vmoo1392@gmail.com>
2020-02-25 16:30:24 -0800

    reconfigured for typescript

---
+++ server/dist/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const fs  = require('fs');
+//const activity  = require("C:/Users/Victor/VueJs/exemplum_meum/Exemplum_Meum/server/src/stats_class/activity");
+//const stats = require("C:/Users/Victor/VueJs/exemplum_meum/Exemplum_Meum/server/src/stats_class/dailyStats");
+
+const app = express();
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/:act', (req, res) => {
+//for testing purposes
+
+	/*let curr_act = new activity(req.params.act, 10)
+	curr_act.write_Activity()*/
+	let act = req.params.act;
+	//let min = req.params.min;
+	console.log(act);
+
+	res.send('Hello World')
+})
+
+app.post('/astroserver', (req, res) => {
+
+	let curr_act = new activity(req.body.activity, req.body.duration);
+})
+
+app.listen(process.env.PORT || 8081);
